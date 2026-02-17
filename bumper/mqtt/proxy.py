9fe96f17474712b94d59c4ad24adb2ba8956587d
Condorello <andreanottoli@gmail.com>
2026-02-17 13:00:57 +0100

    Update proxy.py
    Moved ConnecteException to ConnectError and ProtocolHandlerException to ProtocolHandlerError

--- bumper/mqtt/proxy.py
+++ bumper/mqtt/proxy.py
@@ -13,11 +13,19 @@ from amqtt.adapters import (
     WebSocketsReader,
     WebSocketsWriter,
 )
-from amqtt.client import ConnectException, MQTTClient
+from amqtt.client import MQTTClient
+from amqtt.errors import ConnectError
 from amqtt.mqtt.connack import CONNECTION_ACCEPTED
 from amqtt.mqtt.constants import QOS_0, QOS_1, QOS_2
 from amqtt.mqtt.protocol.client_handler import ClientProtocolHandler
-from amqtt.mqtt.protocol.handler import ProtocolHandlerException
+
+# compat: alcuni rami/vecchie versioni avevano ProtocolHandlerException,
+# nelle nuove è ProtocolHandlerError
+try:
+    from amqtt.errors import ProtocolHandlerError as _ProtocolHandlerError
+except Exception:  # pragma: no cover
+    _ProtocolHandlerError = Exception  # type: ignore[assignment]
+
 from cachetools import TTLCache
 from websockets.exceptions import InvalidHandshake, InvalidURI
 
@@ -44,7 +52,7 @@ class ProxyClient:
         timeout: float = 180,
     ):
         self.request_mapper: MutableMapping[str, str] = TTLCache(
-            maxsize=timeout * timeout, ttl=timeout * 1.1
+            maxsize=int(timeout * timeout), ttl=timeout * 1.1
         )
         self._client = _NoCertVerifyClient(client_id=client_id, config=config)
         self._host = host
@@ -69,31 +77,31 @@ class ProxyClient:
                 data = message.data.decode("utf-8") if message.data else ""
 
                 _LOGGER.info(
-                    f"Message Received From Ecovacs - Topic: {message.topic} - Message: {data}"
+                    "Message Received From Ecovacs - Topic: %s - Message: %s",
+                    message.topic,
+                    data,
                 )
                 topic = message.topic
                 ttopic = topic.split("/")
                 if ttopic[1] == "p2p":
                     if ttopic[3] == "proxyhelper":
-                        _LOGGER.error(
-                            f'"proxyhelper" was sender - INVALID!! Topic: {topic}'
-                        )
+                        _LOGGER.error('"proxyhelper" was sender - INVALID!! Topic: %s', topic)
                         continue
 
                     self.request_mapper[ttopic[10]] = ttopic[3]
                     ttopic[3] = "proxyhelper"
                     topic = "/".join(ttopic)
-                    _LOGGER.info(f"Converted Topic From {message.topic} TO {topic}")
+                    _LOGGER.info("Converted Topic From %s TO %s", message.topic, topic)
 
                 _LOGGER.info(
-                    f"Proxy Forward Message to Robot - Topic: {topic} - Message: {data}"
+                    "Proxy Forward Message to Robot - Topic: %s - Message: %s",
+                    topic,
+                    data,
                 )
 
                 bumper.mqtt_helperbot.publish(topic, message.data)
             except Exception:
-                _LOGGER.error(
-                    "An error occurred during handling a message", exc_info=True
-                )
+                _LOGGER.error("An error occurred during handling a message", exc_info=True)
 
     async def subscribe(self, topic: str, qos: QOS_0 | QOS_1 | QOS_2 = QOS_0) -> None:
         """Subscribe to topic."""
@@ -111,19 +119,20 @@ class ProxyClient:
 class _NoCertVerifyClient(MQTTClient):  # type:ignore[misc]
     # pylint: disable=all
     """
-    Mqtt client, which is not verify the certificate.
+    Mqtt client, which does not verify the certificate.
 
-    Purpose is only to add "sc.verify_mode = ssl.CERT_NONE  # Ignore verify of cert"
+    Purpose is only to add:
+        sc.verify_mode = ssl.CERT_NONE
     """
 
     @typing.no_type_check
     async def _connect_coro(self):
         kwargs = dict()
 
-        # Decode URI attributes
         uri_attributes = urlparse(self.session.broker_uri)
         scheme = uri_attributes.scheme
         secure = True if scheme in ("mqtts", "wss") else False
+
         self.session.username = (
             self.session.username if self.session.username else uri_attributes.username
         )
@@ -132,12 +141,13 @@ class _NoCertVerifyClient(MQTTClient):  # type:ignore[misc]
         )
         self.session.remote_address = uri_attributes.hostname
         self.session.remote_port = uri_attributes.port
+
         if scheme in ("mqtt", "mqtts") and not self.session.remote_port:
             self.session.remote_port = 8883 if scheme == "mqtts" else 1883
         if scheme in ("ws", "wss") and not self.session.remote_port:
             self.session.remote_port = 443 if scheme == "wss" else 80
+
         if scheme in ("ws", "wss"):
-            # Rewrite URI to conform to https://tools.ietf.org/html/rfc6455#section-3
             uri = (
                 scheme,
                 self.session.remote_address + ":" + str(self.session.remote_port),
@@ -147,8 +157,7 @@ class _NoCertVerifyClient(MQTTClient):  # type:ignore[misc]
                 uri_attributes[5],
             )
             self.session.broker_uri = urlunparse(uri)
-        # Init protocol handler
-        # if not self._handler:
+
         self._handler = ClientProtocolHandler(self.plugins_manager)
 
         if secure:
@@ -160,9 +169,7 @@ class _NoCertVerifyClient(MQTTClient):  # type:ignore[misc]
             )
             if "certfile" in self.config and "keyfile" in self.config:
                 sc.load_cert_chain(self.config["certfile"], self.config["keyfile"])
-            if "check_hostname" in self.config and isinstance(
-                self.config["check_hostname"], bool
-            ):
+            if "check_hostname" in self.config and isinstance(self.config["check_hostname"], bool):
                 sc.check_hostname = self.config["check_hostname"]
 
             sc.verify_mode = ssl.CERT_NONE  # Ignore verify of cert
@@ -172,13 +179,14 @@ class _NoCertVerifyClient(MQTTClient):  # type:ignore[misc]
             reader = None
             writer = None
             self._connected_state.clear()
-            # Open connection
+
             if scheme in ("mqtt", "mqtts"):
                 conn_reader, conn_writer = await asyncio.open_connection(
                     self.session.remote_address, self.session.remote_port, **kwargs
                 )
                 reader = StreamReaderAdapter(conn_reader)
                 writer = StreamWriterAdapter(conn_writer)
+
             elif scheme in ("ws", "wss"):
                 websocket = await websockets.connect(
                     self.session.broker_uri,
@@ -188,40 +196,36 @@ class _NoCertVerifyClient(MQTTClient):  # type:ignore[misc]
                 )
                 reader = WebSocketsReader(websocket)
                 writer = WebSocketsWriter(websocket)
-            # Start MQTT protocol
+
             self._handler.attach(self.session, reader, writer)
             return_code = await self._handler.mqtt_connect()
+
             if return_code is not CONNECTION_ACCEPTED:
                 self.session.transitions.disconnect()
-                self.logger.warning("Connection rejected with code '%s'" % return_code)
-                exc = ConnectException("Connection rejected by broker")
-                exc.return_code = return_code
-                raise exc
-            else:
-                # Handle MQTT protocol
-                await self._handler.start()
-                self.session.transitions.connect()
-                self._connected_state.set()
-                self.logger.debug(
-                    "connected to %s:%s"
-                    % (self.session.remote_address, self.session.remote_port)
-                )
+                self.logger.warning("Connection rejected with code '%s'", return_code)
+                raise ConnectError("Connection rejected by broker")
+
+            await self._handler.start()
+            self.session.transitions.connect()
+            self._connected_state.set()
+            self.logger.debug(
+                "connected to %s:%s",
+                self.session.remote_address,
+                self.session.remote_port,
+            )
             return return_code
+
         except InvalidURI as iuri:
-            self.logger.warning(
-                "connection failed: invalid URI '%s'" % self.session.broker_uri
-            )
+            self.logger.warning("connection failed: invalid URI '%s'", self.session.broker_uri)
             self.session.transitions.disconnect()
-            raise ConnectException(
-                "connection failed: invalid URI '%s'" % self.session.broker_uri, iuri
-            )
+            raise ConnectError(f"connection failed: invalid URI '{self.session.broker_uri}'") from iuri
+
         except InvalidHandshake as ihs:
             self.logger.warning("connection failed: invalid websocket handshake")
             self.session.transitions.disconnect()
-            raise ConnectException(
-                "connection failed: invalid websocket handshake", ihs
-            )
-        except (ProtocolHandlerException, ConnectionError, OSError) as e:
-            self.logger.warning("MQTT connection failed: %r" % e)
+            raise ConnectError("connection failed: invalid websocket handshake") from ihs
+
+        except (_ProtocolHandlerError, ConnectionError, OSError) as e:
+            self.logger.warning("MQTT connection failed: %r", e)
             self.session.transitions.disconnect()
-            raise ConnectException(e)
+            raise ConnectError(str(e)) from e
